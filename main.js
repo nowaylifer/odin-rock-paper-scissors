6f3ff4bccd65599ffb7ccb7b7eb9dcc406c9cc2d
Marat Bondarenko <nowaylifer@gmail.com>
2022-12-30 17:14:05 +0300

    Create function that returns computer choice

---
+++ main.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  switch(Math.floor(Math.random() * 3) + 1) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors'
+  }
+}
+
