7d76d75bf881018c100ea06d2a43749bc35c2556
Marat Bondarenko <nowaylifer@gmail.com>
2022-12-30 17:49:11 +0300

    Add playRound function

--- main.js
+++ main.js
@@ -11,5 +11,16 @@ function getComputerChoice() {
 
 let playerSelection = prompt("Take Your Turn");
 playerSelection = playerSelection.toLowerCase();
-
 const computerSelection = getComputerChoice();
+
+function playRound(player, computer) {
+  if (player === computer) { 
+    return 'Tie!';
+  } else if (player === 'rock' && computer === 'scissors' || 
+             player === 'scissors' && computer === 'paper' ||
+             player === 'paper' && computer === 'rock') {
+      return 'You win!';
+  } else {
+      return 'You lose!'
+  }
+}
