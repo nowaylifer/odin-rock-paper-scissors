b5530727a858edde893ad19c69ffebdcfdda3140
Marat Bondarenko <nowaylifer@gmail.com>
2023-01-06 18:41:36 +0300

    !Game function rework
    * Added buttons with eventlisteners instead of prompt
    * Added getRoundResult function
    * Reworked game function

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
+
+
+function game(result) {
+  if (rounds < 5) {
+    getRoundResult(result);
+  } else {
+    getRoundResult(result);
+  
+    if (playerScore > computerScore) {
+      console.log('Player won game') 
+    } else {
+      console.log('Computer won game')
+    }
+    
+    rounds = 0;
+    playerScore = 0;
+    computerScore = 0;
+  }
+}
+
+
+function getRoundResult(result) {
+  switch(result) {
+    case 'tie':
+      playerScore++
+      computerScore++
+      console.log('Tie round');
+      break;
+    case 'player':
+      playerScore++
+      console.log('Player won round');
+      break;
+    case 'computer':
+      console.log('Computer won round');
+      computerScore++
+      break;
+  }
+}
+
+const choiceButtons = document.querySelectorAll('button.choice');
+choiceButtons.forEach(button => {
+  button.addEventListener('click', playRound);
+});
+
+
 function getComputerChoice() {
   switch(Math.floor(Math.random() * 3) + 1) {
     case 1:
@@ -5,31 +53,26 @@ function getComputerChoice() {
     case 2:
       return 'paper';
     case 3:
-      return 'scissors'
-  }
-}
+      return 'scissors';
+  };
+};
 
 
-function playRound() {
-  let playerSelection = prompt("Take Your Turn");
-  playerSelection = playerSelection.toLowerCase();
-  const computerSelection = getComputerChoice();
-  
-  if (playerSelection === computerSelection) { 
-      return 'Tie!';
+function playRound(e) {
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  if (playerChoice === computerChoice) {
+      rounds++
+      game("tie");
   } else if (
-      playerSelection === 'rock' && computerSelection === 'scissors' || 
-      playerSelection === 'scissors' && computerSelection === 'paper' ||
-      playerSelection === 'paper' && computerSelection === 'rock') {
-      return 'You win!';
+      playerChoice === 'rock' && computerChoice === 'scissors' || 
+      playerChoice === 'scissors' && computerChoice === 'paper' ||
+      playerChoice === 'paper' && computerChoice === 'rock') {
+        rounds++
+        game("player"); // player win
   } else {
-      return 'You lose!'
+      rounds++
+      game("computer"); // computer win
   }
 }
 
-
-function game(n) {
-  for (let i = 0; i < n; i++) {
-    console.log(playRound());
- }
-}
