260dd47262d6690e18a69eebb1f444054ed25491
Marat Bondarenko <nowaylifer@gmail.com>
2023-01-07 18:45:15 +0300

    Another huge rework

--- main.js
+++ main.js
@@ -1,78 +1,159 @@
-let playerScore = 0;
-let computerScore = 0;
-let rounds = 0;
-
-
-function game(result) {
-  if (rounds < 5) {
-    getRoundResult(result);
-  } else {
-    getRoundResult(result);
-  
-    if (playerScore > computerScore) {
-      console.log('Player won game') 
-    } else {
-      console.log('Computer won game')
-    }
-    
-    rounds = 0;
-    playerScore = 0;
-    computerScore = 0;
-  }
-}
-
-
-function getRoundResult(result) {
-  switch(result) {
-    case 'tie':
-      playerScore++
-      computerScore++
-      console.log('Tie round');
-      break;
-    case 'player':
-      playerScore++
-      console.log('Player won round');
-      break;
-    case 'computer':
-      console.log('Computer won round');
-      computerScore++
-      break;
-  }
-}
+'use strict'
 
 const choiceButtons = document.querySelectorAll('button.choice');
+let playerScoreShown = document.querySelector('#player-score');
+let cpuScoreShown = document.querySelector('#cpu-score');
+let roundResultShown = document.querySelector('#round-result');
+
+let playerScore = 0;
+let cpuScore = 0;
+
+
 choiceButtons.forEach(button => {
-  button.addEventListener('click', playRound);
+  button.addEventListener('click', clickHandle);
 });
 
 
-function getComputerChoice() {
-  switch(Math.floor(Math.random() * 3) + 1) {
-    case 1:
-      return 'rock';
-    case 2:
-      return 'paper';
-    case 3:
-      return 'scissors';
-  };
-};
+
+function isGameOver() {
+  if (playerScore === 5 || cpuScore === 5) return true;
+  else return false;
+}
 
 
-function playRound(e) {
-  const playerChoice = e.target.id;
-  const computerChoice = getComputerChoice();
-  if (playerChoice === computerChoice) {
-      rounds++
-      game("tie");
-  } else if (
-      playerChoice === 'rock' && computerChoice === 'scissors' || 
-      playerChoice === 'scissors' && computerChoice === 'paper' ||
-      playerChoice === 'paper' && computerChoice === 'rock') {
-        rounds++
-        game("player"); // player win
-  } else {
-      rounds++
-      game("computer"); // computer win
+function clickHandle(e) {
+  if (isGameOver()) return;
+  
+  const choices = ['rock', 'paper', 'scissors'];
+  let playerSelection = e.target.id;
+  let cpuSelection = choices[Math.floor(Math.random() * choices.length)];
+  let winner = playRound(playerSelection, cpuSelection);
+  
+  updateRoundResult(winner, playerSelection, cpuSelection);
+  
+  if ( isGameOver() ) {
+    declareWinner();
+    playAgain();
   }
 }
+
+
+function updateRoundResult(winner, playerSelection, cpuSelection) {
+  updateScores();
+  
+  if (winner === 'tie') {
+    roundResultShown.textContent = `Tie!. You both threw ${playerSelection}.`
+  } else if (winner === 'cpu') {
+    roundResultShown.textContent = `You lose! ${capitalize(cpuSelection)} beats ${playerSelection}.`;
+  } else if (winner === 'player') {
+    roundResultShown.textContent = `You win! ${capitalize(playerSelection)} beats ${cpuSelection}.`;
+  }
+}
+
+
+function declareWinner() {
+  let winnerShown = document.createElement('div');
+  winnerShown.id = 'winner-shown';
+  
+  if (playerScore > cpuScore) {
+    winnerShown.textContent = "You win game! Congrats!";
+  } else {
+    winnerShown.textContent = "You lose game! Shame!";
+  }
+
+  document.body.appendChild(winnerShown);
+}
+
+
+function playAgain() {
+  let playAgainButton = document.createElement('button');
+  playAgainButton.textContent = "Play again?";
+
+  document.body.appendChild(playAgainButton);
+
+  playAgainButton.addEventListener('click', () => {
+    document.getElementById('winner-shown').remove();
+    roundResultShown.textContent = "";
+    playerScore = 0;
+    cpuScore = 0;
+    updateScores();
+    playAgainButton.remove();
+  });
+}
+
+
+function updateScores() {
+  cpuScoreShown.textContent = `${cpuScore}`;
+  playerScoreShown.textContent = `${playerScore}`;
+}
+
+
+function capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playRound(playerSelection, cpuSelection) {
+  let whoIsWinner;
+  const cpuWinCases = [ 
+    ['rock', 'scissors'], 
+    ['scissors', 'paper'], 
+    ['paper', 'rock'] 
+  ];
+  
+  if (playerSelection === cpuSelection) {
+    whoIsWinner = 'tie';
+    cpuScore++
+    playerScore++
+  } else if (function isPlayerWin() {
+      for (let i = 0; i < cpuWinCases; i++) {
+        if (cpuSelection === cpuWinCases[i][0] && playerSelection === cpuWinCases[i][1]) {
+          return true;
+        }
+      }
+      return false;
+    }) {
+      whoIsWinner = 'cpu';
+      cpuScore++
+  } else {
+      whoIsWinner = 'player';
+      playerScore++
+  }
+
+  return whoIsWinner;
+}
+
+
+
+
 
